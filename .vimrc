68068f4ac1b0233f06a0b5a374eb26dfa98a713c
Maxim Nurmukhametov <maxim.nurmukhametov@gmail.com>
2020-02-03 17:50:23 +0500

    add vim extensions installing

--- .vimrc
+++ .vimrc
@@ -7,17 +7,41 @@ python3 powerline_setup()
 python3 del powerline_setup
 set laststatus=2
 
+"russian shortcuts works in command mode (commands don't)
 set langmap=ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz
 
+"colorscheme
 let g:solarized_termcolors=256
 set background=dark
 colorscheme solarized
+
 set number
 set cursorline
 set wildmenu
 set showmatch
 set incsearch
 set hlsearch
+
 set tabstop=4
 set softtabstop=4
-set expandtab     
+set expandtab
+
+set foldmethod=indent
+set foldlevel=99
+
+"folding with space
+nnoremap <space> za
+"split navigations
+nnoremap <C-J> <C-W><C-J>
+nnoremap <C-K> <C-W><C-K>
+nnoremap <C-L> <C-W><C-L>
+nnoremap <C-H> <C-W><C-H>
+
+au BufNewFile,BufRead *.py
+    \ set tabstop=4
+    \ set softtabstop=4
+    \ set shiftwidth=4
+    \ set textwidth=79
+    \ set expandtab
+    \ set autoindent
+    \ set fileformat=unix
--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# function to install smthng from apt if its not installed
 function install () {
         if which $1
         then
@@ -8,4 +9,39 @@ function install () {
         fi
 }
 
-install i3
+#constants
+readonly CURDIR=$PWD
+readonly PLUGINS="/home/maxim/.vim/pack/plugins/start"
+readonly INDENT="/home/maxim/.vim/indent"
+echo $PWD $PLUGINS
+
+# install dotfiles
+cd ~/dotfiles/
+cp .vimrc .bashrc ~/
+mkdir -p $PLUGINS
+mkdir $INDENT
+cd $PLUGINS
+
+#install vim extensions
+if [ -d $PLUGINS ]
+then
+	git clone git@github.com:jiangmiao/auto-pairs.git
+	git clone git@github.com:mattn/emmet-vim.git
+	git clone git@github.com:altercation/vim-colors-solarized.git
+    git clone git@github.com:Konfekt/FastFold.git
+    git clone git@github.com:tmhedberg/SimpylFold.git
+fi
+
+#install vim indent files
+if [ -d $INDENT  ]
+then
+        cd $INDENT
+        curl -LO 'https://raw.githubusercontent.com/vim-scripts/indentpython.vim/master/indent/python.vim'
+fi
+
+cd $CURDIR 
+echo "vim extensions installed:"
+ls $PLUGINS
+echo "dotfiles installed:"
+ls ~/.bashrc ~/.vimrc
+#TODO unset const
